e29473b08e88cb5b4a4df7181b253ef8649a34d0
UWellFail <46130993+UWellFail@users.noreply.github.com>
2019-11-13 16:16:32 +0300

    Update ci-setup.sh

--- etc/ci-setup.sh
+++ etc/ci-setup.sh
@@ -22,6 +22,10 @@ EOF
 
 sudo apt-get -qq update
 sudo apt-get install software-properties-common -y
+sudo apt-get update
+sudo apt-get install libssl1.0.0 libssl-dev
+sudo ln -s /lib/x86_64-linux-gnu/libssl.so.1.0.0 /lib/x86_64-linux-gnu/libssl.so.10
+sudo ln -s /lib/x86_64-linux-gnu/libcrypto.so.1.0.0 /lib/x86_64-linux-gnu/libcrypto.so.10
 curl --fail --retry 3 --retry-delay 1 --connect-timeout 3 --max-time 30 https://cli-assets.heroku.com/install-ubuntu.sh | sh
 
 if [ -n "$HEROKU_API_KEY" ]; then
